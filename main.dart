40c61885210eeef263b2222c009de4ddcc69eeea
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-05-18 22:15:51 +0600

    => operator for returning expression.

--- main.dart
+++ main.dart
@@ -1,18 +1,19 @@
 main() {
 
-  var name = showName();
-  int Age = age();
-  print("Hello ${showName()} $Age ");
+  var name = getName();
+  int Age = simplifyAge();
+  print("Hello $name $Age ");
   print("is it true? ${isknow()}");
 
   doSomething();  //call function in main function.
 }
 
-String showName()                           //return type string
+String showName()                           //return type string. We can simplify this code by using => notation that we see in below.
 {
   return "I am Antor.";
 }
-
+String getName() => "Rifat Anjum";   // => means Returning the Expression.
+int simplifyAge() => 18;
 int age()           //return type int
 {
   return 27;
@@ -26,7 +27,7 @@ bool isknow()       //return type bool
   } else
     return false;
 }
-doSomething()                         //function
+ void doSomething()                         //void type nothing return.
 {
   print("I am a developer.");
 }
